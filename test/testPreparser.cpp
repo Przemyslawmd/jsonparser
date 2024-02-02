98677c618f4f33d9401caba2f56f7ff82f9e46ab
przemyslawmd <przemyslawmd@gmail.com>
2024-02-02 22:01:48 +0100

    First test

--- test/testPreparser.cpp
+++ test/testPreparser.cpp
@@ -1,11 +1,20 @@
 
 #include "../src/Preparser.h"
+#include "config.h"
+
+#include <fstream>
 
 #include <gtest/gtest.h>
 
 
 TEST (PreparserTest, FirstTest)
 {
-    std::unique_ptr<Preparser> preparser = std::make_unique<Preparser>(); 
+    std::unique_ptr<Preparser> preparser = std::make_unique<Preparser>();
+
+    std::string filePath = std::string(TEST_DATA) + "/test.json";
+    std::ifstream jsonFile(filePath);
+    std::string jsonStr((std::istreambuf_iterator<char>(jsonFile)),
+                         std::istreambuf_iterator<char>());
+    preparser->parseJSON(jsonStr);
 }
 
