03bff1a2b54913297d89ffa1da3ab9f986c8fdd5
przemyslawmd <przemyslawmd@gmail.com>
2024-03-16 18:00:21 +0100

    Tests for preparsing

---
+++ config.h
@@ -0,0 +1,6 @@
+
+#pragma once
+
+#define TEST__DATA "D:/Projects/jsoncpp/"
+#define IMPROPER_BOARD_DIR "D:/Projects/jsoncpp/improperBoards/"
+#define IMAGES_DIR "D:/Projects/jsoncpp/images/"
--- test/testPreparser.cpp
+++ test/testPreparser.cpp
@@ -12,13 +12,14 @@
 struct TestData 
 {
     TokenType type;
-    std::variant<std::string, int, float, bool, nullptr_t> data;
+    std::variant<std::string, int, nullptr_t> data;
 };
 
 
-void checkTokens(std::unique_ptr<std::vector<Token>> tokens, std::vector<TestData>& testData)
+void checkTokens(std::unique_ptr<std::vector<PreToken>> tokens, std::vector<TestData>& testData)
 {
     ASSERT_EQ(tokens->size(), testData.size());
+    
     for (int i = 0; i < tokens->size(); i++) {
         ASSERT_EQ(tokens->at(i).type, testData[i].type );
         if (tokens->at(i).type == TokenType::DATA_INT) {
@@ -27,17 +28,17 @@ void checkTokens(std::unique_ptr<std::vector<Token>> tokens, std::vector<TestDat
         else if (tokens->at(i).type == TokenType::DATA_STR) {
             ASSERT_EQ(std::get<std::string>(tokens->at(i).data), std::get<std::string>(testData[i].data));
         }
-    } 
+    }
 }
 
 
 TEST (PreparserTest, FirstTest)
 {
-    std::string filePath = std::string(TEST_DATA) + "/test.json";
+    std::string filePath = std::string(TEST_DATA) + "/test_1.json";
     std::ifstream jsonFile(filePath);
     std::string jsonStr((std::istreambuf_iterator<char>(jsonFile)),
-                         std::istreambuf_iterator<char>());
-    
+    std::istreambuf_iterator<char>());
+
     auto preparser = std::make_unique<Preparser>();
     auto tokens = preparser->parseJSON(jsonStr);
 
@@ -54,11 +55,110 @@ TEST (PreparserTest, FirstTest)
        { TokenType::COLON} ,
        { TokenType::DATA_INT, 39 },
        { TokenType::COMMA },
-       { TokenType::DATA_STR, std::string {"country" }},
+       { TokenType::DATA_STR, std::string{ "country" }},
        { TokenType::COLON },
-       { TokenType::DATA_STR, std::string {"Poland" }},
+       { TokenType::DATA_STR, std::string{ "Poland" }},
        { TokenType::CURLY_CLOSE },
        { TokenType::CURLY_CLOSE },
+    };
+
+    checkTokens(std::move(tokens), testData);
+}
+
+
+TEST(PreparserTest, SecondTest)
+{
+    std::string filePath = std::string(TEST_DATA) + "/test_2.json";
+    std::ifstream jsonFile(filePath);
+    std::string jsonStr((std::istreambuf_iterator<char>(jsonFile)),
+        std::istreambuf_iterator<char>());
+
+    auto preparser = std::make_unique<Preparser>();
+    auto tokens = preparser->parseJSON(jsonStr);
+
+    std::vector<TestData> testData = {
+       { TokenType::CURLY_OPEN, nullptr },
+       
+       { TokenType::DATA_STR, std::string{ "name" }},
+       { TokenType::COLON, nullptr },
+       { TokenType::DATA_STR, std::string{ "John Smith" }},
+       { TokenType::COMMA }, //5
+       
+       { TokenType::DATA_STR, std::string{ "value" }},
+       { TokenType::COLON, nullptr },
+       { TokenType::DATA_STR, std::string{ "20223" }},
+       { TokenType::COMMA },
+
+       { TokenType::DATA_STR, std::string{ "price" }},
+       { TokenType::COLON, nullptr },
+       { TokenType::DATA_INT, 2224 },
+       { TokenType::COMMA },
+
+       { TokenType::DATA_STR, std::string{ "shipTo" }},
+       { TokenType::COLON, nullptr },
+       { TokenType::CURLY_OPEN, nullptr },
+
+       { TokenType::DATA_STR, std::string{ "name" }},
+       { TokenType::COLON, nullptr },
+       { TokenType::DATA_STR, std::string{ "Jane Smith" }},
+       { TokenType::COMMA },
+
+       { TokenType::DATA_STR, std::string{ "address" }},
+       { TokenType::COLON, nullptr },
+       { TokenType::DATA_STR, std::string{ "123 Maple Street" }},
+       { TokenType::COMMA },
+
+       { TokenType::DATA_STR, std::string{ "city" }},
+       { TokenType::COLON, nullptr },
+       { TokenType::DATA_STR, std::string{ "Pretendville" }},
+       { TokenType::COMMA },
+
+       { TokenType::DATA_STR, std::string{ "state" }},
+       { TokenType::COLON, nullptr },
+       { TokenType::DATA_STR, std::string{ "NY" }},
+       { TokenType::COMMA },
+
+       { TokenType::DATA_STR, std::string{ "zip" }},
+       { TokenType::COLON, nullptr },
+       { TokenType::DATA_INT, 12345},
+
+
+       { TokenType::CURLY_CLOSE, nullptr },
+       { TokenType::COMMA },
+
+       { TokenType::DATA_STR, std::string{ "billTo" }},
+       { TokenType::COLON, nullptr },
+       { TokenType::CURLY_OPEN, nullptr },
+
+       { TokenType::DATA_STR, std::string{ "name" }},
+       { TokenType::COLON, nullptr },
+       { TokenType::DATA_STR, std::string{ "John Smith" }},
+       { TokenType::COMMA },
+       
+       
+
+       
+       { TokenType::DATA_STR, std::string{ "address" }},
+       { TokenType::COLON, nullptr },
+       { TokenType::DATA_STR, std::string{ "123 Maple Street" }},
+       { TokenType::COMMA },
+
+       { TokenType::DATA_STR, std::string{ "city" }},
+       { TokenType::COLON, nullptr },
+       { TokenType::DATA_STR, std::string{ "Pretendville" }},
+       { TokenType::COMMA },
+
+       { TokenType::DATA_STR, std::string{ "state" }},
+       { TokenType::COLON, nullptr },
+       { TokenType::DATA_STR, std::string{ "NY" }},
+       { TokenType::COMMA },
+
+       { TokenType::DATA_STR, std::string{ "zip" }},
+       { TokenType::COLON, nullptr },
+       { TokenType::DATA_INT, 12345},
+
+       { TokenType::CURLY_CLOSE, nullptr },
+       { TokenType::CURLY_CLOSE, nullptr },
     };
 
     checkTokens(std::move(tokens), testData);
