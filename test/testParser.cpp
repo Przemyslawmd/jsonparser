d7821f074c018b0af168ddbf5443dcf177616e49
przemyslawmd <przemyslawmd@gmail.com>
2024-03-24 23:54:40 +0100

    New test; tests refactoring

--- test/testParser.cpp
+++ test/testParser.cpp
@@ -11,6 +11,31 @@
 
 #include <gtest/gtest.h>
 
+
+void checkStringNode(std::map<std::string, NodeValue>* nodePointer, const std::string& key, const std::string& value)
+{ 
+    auto* node = std::get_if<std::string>(&(nodePointer->at(key).value));
+    ASSERT_TRUE(node != nullptr);
+    ASSERT_EQ(*node, value);
+}
+
+
+void checkIntNode(std::map<std::string, NodeValue>* nodePointer, const std::string& key, int value)
+{
+    auto* node = std::get_if<int>(&(nodePointer->at(key).value));
+    ASSERT_TRUE(node != nullptr);
+    ASSERT_EQ(*node, value);
+}
+
+
+void checkBoolNode(std::map<std::string, NodeValue>* nodePointer, const std::string& key, bool value)
+{
+    auto* node = std::get_if<bool>(&(nodePointer->at(key).value));
+    ASSERT_TRUE(node != nullptr);
+    ASSERT_EQ(*node, value);
+}
+
+
 TEST(ParserTest, FirstTest)
 {
     std::string filePath = std::string(TEST_DATA) + "/test_1.json";
@@ -30,17 +55,45 @@ TEST(ParserTest, FirstTest)
     ASSERT_TRUE(nodePerson->find("name") != nodePerson->end());
     ASSERT_TRUE(nodePerson->find("age") != nodePerson->end());
     ASSERT_TRUE(nodePerson->find("country") != nodePerson->end());
+    ASSERT_TRUE(nodePerson->find("employed") != nodePerson->end());
+    ASSERT_TRUE(nodePerson->find("restricted") != nodePerson->end());
 
-    auto* nodeName = std::get_if<std::string>(&(nodePerson->at("name").value));
-    ASSERT_TRUE(nodeName != nullptr);
-    ASSERT_EQ(*nodeName, "John");
+    checkStringNode(nodePerson, "name", "John");
+    checkIntNode(nodePerson, "age", 39);
+    checkStringNode(nodePerson, "country", "Poland");
+    checkBoolNode(nodePerson, "employed", true);
+    checkBoolNode(nodePerson, "restricted", false);
+}
 
-    auto* nodeAge = std::get_if<int>(&(nodePerson->at("age").value));
-    ASSERT_TRUE(nodeAge != nullptr);
-    ASSERT_EQ(*nodeAge, 39);
 
-    auto* nodeCountry = std::get_if<std::string>(&(nodePerson->at("country").value));
-    ASSERT_TRUE(nodeCountry != nullptr);
-    ASSERT_EQ(*nodeCountry, "Poland");
+TEST(ParserTest, SecondTest)
+{
+    std::string filePath = std::string(TEST_DATA) + "/test_3.json";
+    std::ifstream jsonFile(filePath);
+    std::string jsonStr((std::istreambuf_iterator<char>(jsonFile)), std::istreambuf_iterator<char>());
+
+    const auto preparser = std::make_unique<Preparser>();
+    const auto tokens = preparser->parseJSON(jsonStr);
+    const auto parser = std::make_unique<Parser>();
+    auto root = parser->parseInitialTokens(*(tokens.get()));
+
+    ASSERT_TRUE(root->find("person") != root->end());
+
+    auto* nodePerson = std::get_if<std::map<std::string, NodeValue>>(&(root->at("person").value));
+    ASSERT_TRUE(nodePerson != nullptr);
+
+    ASSERT_TRUE(nodePerson->find("name") != nodePerson->end());
+    ASSERT_TRUE(nodePerson->find("age") != nodePerson->end());
+    ASSERT_TRUE(nodePerson->find("country") != nodePerson->end());
+
+    checkStringNode(nodePerson, "name", "John");
+    checkIntNode(nodePerson, "age", 39);
+    checkStringNode(nodePerson, "country", "Poland");
+
+    ASSERT_TRUE(root->find("company") != root->end());
+    ASSERT_TRUE(root->find("city") != root->end());
+
+    checkStringNode(root.get(), "company", "abc");
+    checkStringNode(root.get(), "city", "Cracow");
 }
 
