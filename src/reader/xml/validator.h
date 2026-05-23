998d7d389a6a949860d2105a5103591796ff514a
przemyslawmd <przemyslawmd@gmail.com>
2026-05-24 00:48:57 +0200

    [XML][Validator] Attributes checked

--- src/reader/xml/validator.h
+++ src/reader/xml/validator.h
@@ -8,10 +8,42 @@
 #include "log/ErrorStorage.h"
 
 
+static bool ValidateAttrs(const std::vector<TokenXML>& attrs)
+{
+    using enum TokenTypeXML;
+
+    if (attrs[0].type != DATA_STR) {
+        ErrorStorage::putError(ErrorCode::XML_VALIDATOR_ATTR);
+        return false;
+    }
+
+    TokenTypeXML prev = DATA_STR;
+    for (const auto& attr : attrs | std::views::drop(1)) {
+        if ((attr.type == DATA_STR && prev != DATA_STR_QUOTA) ||
+            (attr.type == EQUAL && prev != DATA_STR) ||
+            (attr.type == DATA_STR_QUOTA && prev != EQUAL)) {
+                ErrorStorage::putError(ErrorCode::XML_VALIDATOR_ATTR);
+                return false;
+        }
+        prev = attr.type;
+    }
+
+    if (prev != DATA_STR_QUOTA) {
+        ErrorStorage::putError(ErrorCode::XML_VALIDATOR_ATTR);
+        return false;
+    }
+    return true;
+}
+
+
 static bool ValidateElems(std::vector<Elem>& elems)
 {
     uint skipFirst = 0;
     if (elems.at(0).type == ElemType::DECLARATION) {
+        if (!elems.at(0).attr.empty() && !ValidateAttrs(elems.at(0).attr)) {
+            ErrorStorage::putError(ErrorCode::XML_VALIDATOR_ATTR);
+            return false;
+        }
         skipFirst = 1;
     }
 
@@ -23,6 +55,9 @@ static bool ValidateElems(std::vector<Elem>& elems)
             return false;
         }
         if (elem.type == ElemType::TAG_OPEN) {
+            if (!elem.attr.empty() && !ValidateAttrs(elem.attr)) {
+                return false;
+            }
             tags.push(elem.name.value());
             continue;
         }
--- test/xml/testValidator.cpp
+++ test/xml/testValidator.cpp
@@ -85,3 +85,47 @@ TEST_F(TestValidatorXML, MismatchedTag_4)
     ASSERT_EQ(errors.at(0).getCode(), ErrorCode::XML_VALIDATOR_MISMATCHED_TAG);
 }
 
+
+TEST_F(TestValidatorXML, Attr_1)
+{
+    auto elems = createElements(TEST_DATA_IMPROPER_XML, "attrs_1.xml");
+    ASSERT_NE(elems, nullptr);
+    bool res = ValidateElems(*elems);
+    ASSERT_FALSE(res);
+    const auto& errors = ErrorStorage::getErrors();
+    ASSERT_EQ(errors.at(0).getCode(), ErrorCode::XML_VALIDATOR_ATTR);
+}
+
+
+TEST_F(TestValidatorXML, Attr_2)
+{
+    auto elems = createElements(TEST_DATA_IMPROPER_XML, "attrs_2.xml");
+    ASSERT_NE(elems, nullptr);
+    bool res = ValidateElems(*elems);
+    ASSERT_FALSE(res);
+    const auto& errors = ErrorStorage::getErrors();
+    ASSERT_EQ(errors.at(0).getCode(), ErrorCode::XML_VALIDATOR_ATTR);
+}
+
+
+TEST_F(TestValidatorXML, Attr_3)
+{
+    auto elems = createElements(TEST_DATA_IMPROPER_XML, "attrs_3.xml");
+    ASSERT_NE(elems, nullptr);
+    bool res = ValidateElems(*elems);
+    ASSERT_FALSE(res);
+    const auto& errors = ErrorStorage::getErrors();
+    ASSERT_EQ(errors.at(0).getCode(), ErrorCode::XML_VALIDATOR_ATTR);
+}
+
+
+TEST_F(TestValidatorXML, Attr_4)
+{
+    auto elems = createElements(TEST_DATA_IMPROPER_XML, "attrs_4.xml");
+    ASSERT_NE(elems, nullptr);
+    bool res = ValidateElems(*elems);
+    ASSERT_FALSE(res);
+    const auto& errors = ErrorStorage::getErrors();
+    ASSERT_EQ(errors.at(0).getCode(), ErrorCode::XML_VALIDATOR_ATTR);
+}
+
