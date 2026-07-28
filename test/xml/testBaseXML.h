edcb1d6f0568f04624b7593899f8cc4e4558813c
przemyslawmd <przemyslawmd@gmail.com>
2026-07-28 20:10:10 +0200

    [XML][Tests] Base class refactoring

--- test/xml/testBaseXML.h
+++ test/xml/testBaseXML.h
@@ -22,21 +22,10 @@ namespace xml
 class TestBaseXML : public testing::Test
 {
 protected:
-
-    TestBaseXML()
-    {
-        const auto fullPath = std::string(TEST_DATA) + "performance.txt";
-        performace.open(fullPath, std::ios::app);
-    }
-
-    ~TestBaseXML()
-    {
-        performace.close();
-    }
-    
     virtual void SetUp()
     {
         ErrorStorage::clear();
+        checkPerformance = false;
     }
 
     std::unique_ptr<std::vector<Token>> createTokens(const std::string& path, const std::string& file)
@@ -64,12 +53,20 @@ protected:
 
     void showDuration(const TIME_TYPE start, const TIME_TYPE end)
     {
+        if (!checkPerformance) {
+            return;
+        }
         const char* testCase = ::testing::UnitTest::GetInstance()->current_test_info()->test_case_name();
         const char* testName = ::testing::UnitTest::GetInstance()->current_test_info()->name();
         auto elapsed = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
         std::cout << "\n############ " << testCase << " : " << testName << " : time: " << elapsed.count() << std::endl << std::endl;
+        const auto fullPath = std::string(TEST_DATA) + "performance.txt";
+        performace.open(fullPath, std::ios::app);
         performace << std::left << std::setw(20) << testCase << std::setw(35) << testName << "time: " << elapsed.count() << std::endl;
+        performace.close();
     }
+
+    bool checkPerformance;
 
 private:
     std::ofstream performace;
